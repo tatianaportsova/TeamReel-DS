6c7e946ef9d98c0d712c2c561dea2e7c046be469
chrishuskey <chrishuskey@users.noreply.github.com>
2020-06-16 13:00:40 -0400

    move all models to /models subdirectory in parent directory (instead of separately stored inside each of our subpackages), and adjust path to load models accordingly in audio_functions.py module

--- audio_analysis/audio_functions.py
+++ audio_analysis/audio_functions.py
@@ -1,8 +1,16 @@
+#!python
+# Author: Tatiana Portsova
+
+"""
+Module of functions for audio processing and ML audio analysis for
+TeamReel videos.
+"""
+
 # Import external modules, packages, libraries we will use:
 from textblob import TextBlob
 from gensim.utils import simple_preprocess
 import shutil
-import re 
+import re
 import speech_recognition as sr
 import os
 import moviepy.editor
@@ -22,31 +30,40 @@ import librosa
 # Import internal modules, packages, libraries for this project:
 from data_infra.data_pipelines import get_next_video
 
-"""
-Load models
-"""
-# loading json and model architecture 
-json_file = open('Pickled_files/MLModel.json', 'r')
+
+# LOAD MODELS:
+
+# Get relative file path for models:
+cwd = os.getcwd().split('/')[-1]
+
+if cwd == 'video-journal-for-teams-ds':
+    MODELS_PATH = './models/'
+elif cwd == 'audio_analysis':
+    MODELS_PATH = '../models/'
+
+print(f"MODELS_PATH is: {MODELS_PATH}")
+
+# loading json and model architecture
+json_file = open(MODELS_PATH + 'audio_analysis_model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 
 # load weights into new model
-loaded_model.load_weights("Pickled_files/Augmented_Model.h5")
- 
+loaded_model.load_weights(MODELS_PATH + 'audio_analysis_weights.h5')
+
 # compile loaded model
 opt = keras.optimizers.rmsprop(lr=0.00001, decay=1e-6)
 loaded_model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
-# load lables
-infile = open('Pickled_files/labels','rb')
+# load class labels
+infile = open(MODELS_PATH + 'audio_analysis_labels','rb')
 lb = pickle.load(infile)
 infile.close()
 
 
 
-'''  Functions for Sentiment Analysis  '''
-
+# FUNCTIONS FOR SENTIMENT ANALYSIS:
 
 def get_audio_from_video(video_file):
     """
@@ -55,7 +72,7 @@ def get_audio_from_video(video_file):
     video = moviepy.editor.VideoFileClip(video_file)
     audio = video.audio
     audio.write_audiofile('audio.wav')
-    
+
 def get_sentiment_analysis():
     """
     A function to get sentiment/emotion analysis from the audio
@@ -64,7 +81,7 @@ def get_sentiment_analysis():
     audio = AudioSegment.from_file("audio.wav", "wav")
     chunk_length_ms = 4000
     chunks = make_chunks(audio, chunk_length_ms)
-    
+
     #Export all of the individual chunks as wav files
     path = "audio_sentiment/"
     try:
@@ -73,14 +90,14 @@ def get_sentiment_analysis():
         print ("Creation of the directory %s failed" % path)
     else:
         print ("Successfully created the directory %s " % path)
-        
+
     for i, chunk in enumerate(chunks):
         chunk_name = "audio_sentiment/chunk{0}.wav".format(i)
         chunk.export(chunk_name, format="wav")
-    
+
     lists_of_files = os.listdir(path)
     test_predictions = pd.DataFrame(columns=['predictions'])
-    
+
     for filename in lists_of_files:
         try:
             data, sample_rate = librosa.load("audio_sentiment/{}".format(filename),
@@ -90,11 +107,11 @@ def get_sentiment_analysis():
             sample_rate = np.array(sample_rate)
             mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate, n_mfcc=13),axis=0)
             newdf = pd.DataFrame(data=mfccs).T
-            
+
             # Apply predictions
             newdf= np.expand_dims(newdf, axis=2)
             newpred = loaded_model.predict(newdf,batch_size=16,verbose=1)
-            
+
             # Get the final predicted label
             final = newpred.argmax(axis=1)
             final = final.astype(int).flatten()
@@ -103,31 +120,30 @@ def get_sentiment_analysis():
             test_predictions.loc[filename] = final
         except:
             pass
-        
+
     labels = test_predictions.predictions.value_counts(normalize=True).keys().tolist()
-    
+
     values= []
-    predictions = {} 
+    predictions = {}
     for i in test_predictions.predictions.value_counts(normalize=True).tolist():
-        values.append(round(i,2))   
+        values.append(round(i,2))
     for key in labels:
         for value in values:
             predictions[key] = value
-            values.remove(value) 
+            values.remove(value)
             break
-            
+
     return predictions
 
 
 
-'''   Functions for Speed of Speech   '''
-
+# FUNCTIONS FOR SPEECH RECOGNITION AND AUDIO TRANSCRIPTION:
 
 def break_audio_file(file_name = 'audio.wav'):
     """
     Breaks an audio file into smaller chunks
     """
-    myaudio = AudioSegment.from_file(file_name, "wav") 
+    myaudio = AudioSegment.from_file(file_name, "wav")
     chunk_length_ms = 20000 # pydub calculates in millisec
     chunks = make_chunks(myaudio, chunk_length_ms) #Make chunks of 20 sec
 
@@ -149,12 +165,12 @@ def get_file_paths(dirname):
     """
     Gets file paths from the directory
     """
-    file_paths = []  
+    file_paths = []
     for root, directories, files in os.walk(dirname):
         for filename in files:
             filepath = os.path.join(root, filename)
-            file_paths.append(filepath)  
-    return file_paths 
+            file_paths.append(filepath)
+    return file_paths
 
 def process_file(file):
     """
@@ -164,13 +180,13 @@ def process_file(file):
     a = ''
     with sr.AudioFile(file) as source:
         #r.adjust_for_ambient_noise(source)  # adjust for noisy audio
-        audio = r.record(source)    
+        audio = r.record(source)
         try:
             a =  r.recognize_google(audio)   # recognize_google_cloud for GC API
         except sr.UnknownValueError:
             a = "Google Speech Recognition could not understand audio"
         except sr.RequestError as e:
-            a = "Could not request results from Google Speech Recognition service; {0}".format(e)  
+            a = "Could not request results from Google Speech Recognition service; {0}".format(e)
     return a
 
 def get_text():
@@ -182,16 +198,16 @@ def get_text():
         print ("Creation of the directory %s failed" % path)
     else:
         print ("Successfully created the directory %s " % path)
-    
-    files = get_file_paths(dirname)                             
-    for file in files:                                           
-        (filepath, ext) = os.path.splitext(file)                  
+
+    files = get_file_paths(dirname)
+    for file in files:
+        (filepath, ext) = os.path.splitext(file)
         file_name = os.path.splitext(os.path.basename(file))[0]
         # only interested if extension is '.wav'
-        if ext == '.wav':                                         
+        if ext == '.wav':
             a = process_file(file)
             with open("text_chunks/{}.txt".format(file_name), "w") as f:
-                f.write(a+". ")  
+                f.write(a+". ")
 
 path = r"text_chunks/"
 def get_transcripts_from_audio(audio_file='audio.wav'):
@@ -203,7 +219,7 @@ def get_transcripts_from_audio(audio_file='audio.wav'):
     files = get_file_paths(path)
     files.sort(key=lambda x: int(re.sub('\D', '', x)))
     #sorted(files, key=lambda x: int(re.sub('\D', '', x)))
-    with open('outputfile.txt', 'w+') as f: 
+    with open('outputfile.txt', 'w+') as f:
         for file in files:
             with open(file) as infile:
                 f.write(infile.read()+'\n')
@@ -212,15 +228,18 @@ def get_transcripts_from_audio(audio_file='audio.wav'):
 def tokenize(text):
     return [token for token in simple_preprocess(text)]   # (if token not in STOPWORDS)
 
-def gather_data(path_to_data): 
+def gather_data(path_to_data):
     data = []
     for f in os.listdir(path):
         if os.path.isdir(f) == False:
             if f[-3:] == 'txt':
                 with open(os.path.join(path,f)) as t:
                     text = t.read().strip('\n')
-                    data.append(tokenize(str(text)))       
+                    data.append(tokenize(str(text)))
     return data
+
+
+# FUNCTIONS FOR SPEED OF SPEECH:
 
 def get_audio_duration(file_name = 'audio.wav'):
     with contextlib.closing(wave.open(file_name,'r')) as f:
@@ -228,17 +247,17 @@ def get_audio_duration(file_name = 'audio.wav'):
         rate = f.getframerate()
         duration = (frames / float(rate)) / 60
         return duration
-    
+
 def get_speed_of_speech():
     tokens = gather_data('outputfile.txt')
     count = 0
     for i in tokens:
-        count += len(i)    
+        count += len(i)
     speed_of_speech = count / get_audio_duration(file_name = 'audio.wav')
     return speed_of_speech
 
 
-'''  Function for Text (from Audio) Sentiment Analysis  '''
+# FUNCTIONS FOR TEXT SENTIMENT ANALYSIS (ON TEXT TRANSCRIPT FROM AUDIO):
 
 def get_text_sentiment(file = 'outputfile.txt'):
     with open(file, 'r') as f:
@@ -247,7 +266,7 @@ def get_text_sentiment(file = 'outputfile.txt'):
         return sentiment
 
 
-'''  A function that removes directories and files after we are done with them  ''' 
+# Function that removes directories and files after we are done with them:
 
 def remove_files():
     """
@@ -255,7 +274,7 @@ def remove_files():
     """
     paths = ['audio_chunks/', 'audio_sentiment/', 'text_chunks/']
     os.remove('outputfile.txt')
-    
+
     for i in paths:
         try:
             shutil.rmtree(i)
@@ -263,7 +282,7 @@ def remove_files():
             print("Error: %s : %s" % (i, e.strerror))
 
 
-'''  The main function to analyse the audio  '''
+# Main function to analyse the audio:
 
 def analyse_audio():
     path = r"text_chunks/"
